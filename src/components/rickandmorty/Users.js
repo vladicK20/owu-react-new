20bc7c55140b8d96cbeadd0982c7e8138d1c9a86
vladicK20 <vladvishnevskiy2018@gmail.com>
2022-06-23 19:15:18 +0300

    hw1

--- src/components/rickandmorty/Users.js
+++ src/components/rickandmorty/Users.js
@@ -2,26 +2,25 @@ import {useEffect, useState} from "react";
 import {User} from "./User";
 
 const Users = () => {
-    const {users, setUsers} = useState([])
-
+    const [users, setUsers] = useState([])
     useEffect(() => {
         fetch('https://rickandmortyapi.com/api/character')
-            .then((value) => value.json())
-            .then(value => setUsers(value))
+            .then(value => value.json())
+            .then(value => {
+                setUsers(value)
+            })
     }, []);
 
     return (
         <div>
-            {
-                users.map(user => <User key={user.id}
+            {users.map(user => <User key={user.id}
                                         name={user.name}
                                         id={user.id}
                                         status={user.status}
                                         species={user.species}
                                         gender={user.gender}
                                         img={user.image}
-                />)
-            }
+            />)}
         </div>
     );
 };
